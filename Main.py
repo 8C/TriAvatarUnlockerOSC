969d56899dbc2dbdac29350ea8be570914d481e1
Xo <26947031+8C@users.noreply.github.com>
2023-02-01 07:21:00 -0700

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,23 @@
+import time
+from pythonosc.udp_client import SimpleUDPClient
+IP, PORT = "127.0.0.1", 9000
+client = SimpleUDPClient(IP, PORT) 
+
+pw1 = 12
+pw2 = 34
+pw3 = 45
+
+if __name__ == '__main__':
+   
+    while True:
+
+        client.send_message(f'/avatar/parameters/TRI_Pwd', pw1)
+        time.sleep(0.5)
+        client.send_message(f'/avatar/parameters/TRI_Pwd', pw2)
+        time.sleep(0.5)
+        client.send_message(f'/avatar/parameters/TRI_Pwd', pw3)
+        time.sleep(0.5)
+    else:
+        print('xd')
+        
+
